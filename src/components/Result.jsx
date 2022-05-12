9f80e6c684f74ed4f6cd20e005afc761428e7664
April Archibald <aprilsclearskies@gmail.com>
2022-05-11 17:39:35 -0700

    added ternary

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,19 +4,34 @@ import { useParams } from 'react-router-dom';
 const Result = (props) => {
     const {input, text, background} = useParams();
 
-    if(!isNaN(input)){
-        return (
-            <div>
-                <h1>The number is: {input}</h1>
-            </div>
-        )
-    } else {
-        return (
-            <div style={{backgroundColor: background}}>
+
+    // if(!isNaN(input)){
+    //     return (
+    //         <div>
+    //             <h1>The number is: {input}</h1>
+    //         </div>
+    //     )
+    // } else {
+    //     return (
+    //         <div style={{backgroundColor: background}}>
+    //         <h1 style={{color: text}}>The word is: {input}</h1>
+    //     </div>
+    // )}
+    return(
+      <div>
+        {
+          !isNaN(input) ? (
+          <div>
+            <h1>The number is: {input}</h1>
+          </div>) : (
+          <div style={{backgroundColor: background}}>
             <h1 style={{color: text}}>The word is: {input}</h1>
-        </div>
+          </div>
+          )
+        }
+      </div>
     )
 }
-}
 
-export default Result
+
+export default Result;
